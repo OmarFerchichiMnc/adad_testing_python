fc12034979a9f66775278aaf9adecfe0dc254e7c
OmarFerchichiMnc <Omar.Ferchichi@mnc.aero>
2025-10-21 16:50:17 +0100

    remove not needed import from testcase filter sale by sale ID

--- src/testcase/testcases_filters/audit_track_sale_filter/testcase_audit_track_sale_id_filter.py
+++ src/testcase/testcases_filters/audit_track_sale_filter/testcase_audit_track_sale_id_filter.py
@@ -1,15 +1,6 @@
-from src.configuration.file_injection.file_injection import InjectFile
-from src.configuration.file_injection.inject_try import ExecuteUpload, PrepareUpload
-from src.configuration.file_injection.inject_try import BastionSSMForward, KillBastionSSM
-from src.configuration.utils.utils import adad_paths, sale_and_settlement_molecule_correct_files
-from src.navigation.audit_track.audit_track_navigation import TestPointNavigateToAuditTrackSale
-from src.navigation.data_ingestion.data_ingestion_navigation import TestPointNavigateToFileDashboard
 from src.navigation.navigation_by_link import TestPointNavigateToAuditTrackSaleByLink
 from src.testcase.testcase_main import TestCase
-from src.ui.audit_track.sale_ui import SaleUIAddFilters, SaleUIClickSearch, SaleUIFilterChannelType, SaleUIFilterPaymentDateRange, SaleUIFilterSaleID
-from src.ui.data_ingestion.file_dashboard_ui import CheckIfFileIngested, FileDashboardUIFilterClickSearch, FileDashboardUIFilterFileID
-
-
+from src.ui.audit_track.sale_ui import SaleUIClickSearch, SaleUIFilterSaleID
 
 
 class AuditTrackSalesSaleIDFilters(TestCase):
